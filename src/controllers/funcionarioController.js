008f0e12ebde86405631645f701fd5119bed85b7
Kelvin <167117708+kelvinfff@users.noreply.github.com>
2026-07-06 20:54:45 -0300

    Código atualizado

---
+++ src/controllers/funcionarioController.js
@@ -0,0 +1,14 @@
+
+const Funcionario = require("../models/funcionario")
+
+exports.cadastrar = async(req,res) => {
+    //insert into funcionario values(?,?,?)
+    const funcionario = await Funcionario.create(req.body)
+    res.status(201).json(funcionario)
+}
+
+exports.listar = async(req,res) => {
+    //select * from funcionarios
+    const funcionarios = await Funcionario.findAll()
+    res.status(200).json(funcionarios)
+}
